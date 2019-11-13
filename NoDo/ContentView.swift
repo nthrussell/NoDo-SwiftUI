c152cf595f559eed90b316182cdedf76dcba2304
M A Russell <rredxx@icloud.com>
2019-11-13 10:49:09 +0600

    Initial Commit

---
+++ NoDo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NoDo
+//
+//  Created by Minhajul Russell on 11/13/19.
+//  Copyright © 2019 russell. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
